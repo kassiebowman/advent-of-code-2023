b953e62b71377b333d17c5bcd460c739d719f3be
Kassie Bowman <19256422+kassiebowman@users.noreply.github.com>
2023-12-01 17:30:27 -0800

    Complete day 1 part 1

--- src/main/java/aoc/Day01.java
+++ src/main/java/aoc/Day01.java
@@ -1,11 +1,13 @@
 package aoc;
 
+import com.google.common.base.CharMatcher;
+
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.List;
 
 /**
- * Day 1
+ * Day 1: Trebuchet?!
  *
  * @see <a href="https://adventofcode.com/2023/day/1">AOC 2023 Day 1</a>
  */
@@ -15,6 +17,18 @@ public class Day01
     {
         final List<String> lines = Utils.getInput(fileName);
 
-        return 0;
+        long sum = 0;
+        for (String line : lines)
+        {
+            // Solution derived from: https://www.baeldung.com/java-find-numbers-in-string
+            final int firstNumIndex = CharMatcher.inRange('0', '9').indexIn(line);
+            final int lastNumIndex = CharMatcher.inRange('0', '9').lastIndexIn(line);
+
+            int firstNum = line.charAt(firstNumIndex) - '0';
+            int lastNum = line.charAt(lastNumIndex) - '0';
+            sum += firstNum * 10 + lastNum;
+        }
+
+        return sum;
     }
 }
